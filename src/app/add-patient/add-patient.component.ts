5d376eba2bbf72cae918d15faf1810ec4d79bac5
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-08 12:36:15 +0530

    backend com

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-patient',
@@ -9,10 +10,15 @@ export class AddPatientComponent {
   name =""
   healthHistory = ""
   doctor = ""
-
+constructor(private api:ApiService){}
   readValue = () =>{
     let data:any = {"name":this.name,"healthHistory":this.healthHistory,"doctor":this.doctor}
     console.log(data)
+    this.api.addPatient(data).subscribe(
+      (response)=>{
+        
+      }
+    )
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +33,8 @@ const myRoute : Routes = [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
